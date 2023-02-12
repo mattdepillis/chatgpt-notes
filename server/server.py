161b3f019a81ad88865f075f035e107eb6db6ff8
mattdepillis <mattdepillis@gmail.com>
2023-02-12 15:44:19 -0500

    relative import testing for nested files

--- server/server.py
+++ server/server.py
@@ -2,10 +2,21 @@
 
 """
 # dependencies
+import configparser
 from flask import Flask
 from flask_pymongo import PyMongo
-import configparser
 import os
+import sys
+
+# path management
+paths, cwd = [ "/utils/" ], os.getcwd()
+for path in paths:
+  sys.path.append(cwd + "/utils/")
+
+# import files
+import app_secrets
+import mongo
+
 
 # configuration variables
 config = configparser.RawConfigParser()
@@ -16,8 +27,11 @@ env = config[SECTION]
 # setup flask app
 app = Flask(__name__)
 
+# mongo
+url = mongo.construct_url(env, app_secrets.mpw)
+
 # routes
 @app.route('/')
 def handler():
   print("app running on port 5000")
-  return f"<p>{env['MONGO_URL']}</p>"
+  return f"<p>{env['MONGO_URL_FIRST']}</p>"
