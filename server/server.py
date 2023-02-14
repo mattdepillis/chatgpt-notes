4e54a94af52e0ecd1c600b16d34be654b1de58fb
mattdepillis <mattdepillis@gmail.com>
2023-02-13 22:41:56 -0500

    connected to example mongo database

--- server/server.py
+++ server/server.py
@@ -1,10 +1,10 @@
 """
-
+Main entrypoint for the Flask server - a REST API that connects to MongoDB Atlas datastore.
 """
 # dependencies
 import configparser
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_pymongo import MongoClient
 import os
 import sys
 
@@ -15,23 +15,24 @@ for path in paths:
 
 # import files
 import app_secrets
-import mongo
-
+import mongodb
 
 # configuration variables
 config = configparser.RawConfigParser()
 config.read(r'./config.ini')
-SECTION = 'DEV'
-env = config[SECTION]
+env = config["DEV"]
 
 # setup flask app
 app = Flask(__name__)
 
 # mongo
-url = mongo.construct_url(env, app_secrets.mpw)
+url = mongodb.construct_url(env, app_secrets.mpw)
+mongo = MongoClient(url)
+table = mongo[env["DB"]]["comments"]
 
 # routes
 @app.route('/')
 def handler():
   print("app running on port 5000")
-  return f"<p>{env['MONGO_URL_FIRST']}</p>"
+  comments = table.find_one({ "name" : "John Bishop" })
+  return f"<p>{comments}</p>"
