d61016fd277063a7c58da371060c7fdd7c1f5ff5
mattdepillis <mattdepillis@gmail.com>
2023-02-14 01:10:53 -0500

    first test API call to flask server + sample react frontend code to achieve

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [userData, setUserData] = useState(null)
+
+  const fetchData = () => fetch('/t', { method: 'GET' })
+    .then((resp) => resp.json())
+    .then(data => setUserData(data))
+    .catch(err => console.log(err))
+
+  useEffect(() => { fetchData() }, [])
+
+  useEffect(() => console.log('u',  userData), [userData])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here's my data:
         </p>
+        {userData && <p>{userData.name}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
